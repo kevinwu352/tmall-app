d333e7e6aea5fca289c5f8110c22d08ee5b6f28a
kevin <kevin.wu@hi.com>
2025-02-14 15:31:01 +0800

    004

--- Modules/ModCommon/ModCommon/Foundation/UIColorExt.swift
+++ Modules/ModCommon/ModCommon/Foundation/UIColorExt.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public extension UIColor {
 
-  convenience init(hex: UInt) { // FUNC
+  convenience init(hex: UInt) { // [F]
     self.init(red:   CGFloat(hex >> 16 & 0xff)/255,
               green: CGFloat(hex >> 8  & 0xff)/255,
               blue:  CGFloat(hex       & 0xff)/255,
@@ -20,7 +20,7 @@ public extension UIColor {
     UIColor(red: .random(in: 0..<1), green: .random(in: 0..<1), blue: .random(in: 0..<1), alpha: 1)
   }
 
-  // 图片 scale 为屏幕 scale
+  // scale = screen scale
   var img: UIImage {
     UIGraphicsImageRenderer(size: CGSize(width: 8, height: 8)).image {
       setFill()
--- Modules/ModCommon/ModCommon/Foundation/UIImageExt.swift
+++ Modules/ModCommon/ModCommon/Foundation/UIImageExt.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 public extension UIImage {
+  var original: UIImage {
+    withRenderingMode(.alwaysOriginal)
+  }
+  var template: UIImage {
+    withRenderingMode(.alwaysTemplate)
+  }
+}
+
+public extension UIImage {
   func scaleToFill(_ limit: CGSize) -> UIImage {
     UIGraphicsImageRenderer(size: limit).image {
       draw(in: $0.format.bounds)
@@ -15,30 +24,21 @@ public extension UIImage {
   }
 
   func scaleAspectFit(_ limit: CGSize) -> UIImage {
-    let ratio = min(limit.width / size.width, limit.height / size.height)
-    let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
-    return UIGraphicsImageRenderer(size: newSize).image {
+    let ratio = size.ratio(limit, true)
+    let sz = CGSize(width: size.width * ratio, height: size.height * ratio)
+    return UIGraphicsImageRenderer(size: sz).image {
       draw(in: $0.format.bounds)
     }
   }
 
   func scaleAspectFill(_ limit: CGSize) -> UIImage {
-    let ratio = max(limit.width / size.width, limit.height / size.height)
-    let newRect = CGRect(x: (limit.width - size.width * ratio) / 2,
-                         y: (limit.height - size.height * ratio) / 2,
-                         width: size.width * ratio,
-                         height: size.height * ratio)
+    let ratio = size.ratio(limit, false)
+    let rt = CGRect(x: (limit.width - size.width * ratio) / 2.0,
+                    y: (limit.height - size.height * ratio) / 2.0,
+                    width: size.width * ratio,
+                    height: size.height * ratio)
     return UIGraphicsImageRenderer(size: limit).image { _ in
-      draw(in: newRect)
+      draw(in: rt)
     }
-  }
-}
-
-public extension UIImage {
-  var original: UIImage {
-    withRenderingMode(.alwaysOriginal)
-  }
-  var template: UIImage {
-    withRenderingMode(.alwaysTemplate)
   }
 }
